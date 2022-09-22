dad4dc563193a176f6019d15fac3e9ceb2ed000c
Aman Kumar Gautam <56026080+aman15091999@users.noreply.github.com>
2022-09-22 14:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ import requests
 import pandas as pd
 
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
+    api_key = ""
+    url = "https://api.themoviedb.org/3/movie/{}?api_key={}&language=en-US".format(movie_id, api_key)
     data = requests.get(url)
     data = data.json()
     poster_path = data['poster_path']
